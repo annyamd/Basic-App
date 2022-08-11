1bcfeb79891a319e7e0520b8e2298301cd7f4436
annyamd <miann9920@gmail.com>
2022-08-11 15:07:30 +0300

    set EnterFragment on EnterActivity

--- app/src/main/java/com/example/basicapp/EnterActivity.java
+++ app/src/main/java/com/example/basicapp/EnterActivity.java
@@ -10,5 +10,13 @@ public class EnterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.enter_fragment_container, EnterFragment.class, null)
+                    .commit();
+        }
+
     }
 }
--- app/src/main/java/com/example/basicapp/EnterFragment.java
+++ app/src/main/java/com/example/basicapp/EnterFragment.java
@@ -4,5 +4,7 @@ import androidx.fragment.app.Fragment;
 
 public class EnterFragment extends Fragment {
 
-
+    public EnterFragment() {
+        super(R.layout.fragment_enter);
+    }
 }
